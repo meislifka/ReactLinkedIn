8621c8cdff9f88819b0c62f1360996ac35c70d8c
meislifka <meislifka@gmail.com>
2023-01-20 12:15:27 -0500

    Desturcturing Arrays and Objects

--- Ch04/04_03/start/react-app/src/App.js
+++ Ch04/04_03/start/react-app/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+//desturcturing, grabbing by its key
 
-function App() {
+const [firstCity,second] = ["Tokyo","Tahoe","Villanova"]
+
+console.log(firstCity);
+console.log(second);
+
+function App({library}) {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
