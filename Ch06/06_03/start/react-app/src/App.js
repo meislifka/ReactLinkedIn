d2132f77eb9ab2a087d6ff21f5df4e2cf5834fe4
meislifka <meislifka@gmail.com>
2023-01-23 16:00:28 -0500

    Handling loading states

--- Ch06/06_03/start/react-app/src/App.js
+++ Ch06/06_03/start/react-app/src/App.js
@@ -13,22 +13,32 @@ function GithubUser({ name, location, avatar }) {
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
   useEffect(() => {
+    setLoading(true);
     fetch(
       `https://api.github.com/users/moonhighway`
     )
       .then((response) => response.json())
-      .then(setData);
+      .then(setData)
+      .then(setLoading(false))
+      .catch(setError);
   }, []);
-  if (data)
-    return (
+
+  if(loading) return <h1>Loading...</h1>
+  if(error)
+    return <pre>{JSON.stringify(error)}</pre>;
+    if(!data) return null;
+
+    return ( 
       <GithubUser
         name={data.name}
         location={data.location}
         avatar={data.avatar_url}
       />
     );
-  return <h1>Data</h1>;
 }
 
 export default App;
