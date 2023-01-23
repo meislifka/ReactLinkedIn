bf9907e3a208561626e5bf5a86a57d3bbd10bb93
meislifka <meislifka@gmail.com>
2023-01-23 10:34:34 -0500

    Building a custom Hook

--- Ch05/05_02/start/react-app/src/App.js
+++ Ch05/05_02/start/react-app/src/App.js
@@ -1,31 +1,35 @@
 import "./App.css";
 import { useState } from "react";
 
+//hooks are used for repeatable code
+
+function useInput(initalValue){
+  const[value,setValue] = useState(initalValue);
+  return (
+    {value,onChange: e=>setValue(e.target.value)},
+     () =>setValue(initialValue)
+  );
+
+}
+
 function App() {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useState("#000000");
   const submit = (e) => {
     e.preventDefault();
-    alert(`${title}, ${color}`);
-    setTitle("");
+    alert(`${titleProps.value}, ${colorProps.value}`);
+    resetTitle()
     setColor("#000000");
   };
   return (
     <form onSubmit={submit}>
       <input
-        value={title}
-        onChange={(event) =>
-          setTitle(event.target.value)
-        }
+        {...titleProps}
         type="text"
         placeholder="color title..."
       />
       <input
-        value={color}
-        type="color"
-        onChange={(event) =>
-          setColor(event.target.value)
-        }
+        {...titleProps}
       />
       <button>ADD</button>
     </form>
