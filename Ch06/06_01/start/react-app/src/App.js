9dc54674bdeb15fa0512fc2df2a62c5eafec9d17
meislifka <meislifka@gmail.com>
2023-01-23 15:47:55 -0500

    Fetching data with Hooks

--- Ch06/06_01/start/react-app/src/App.js
+++ Ch06/06_01/start/react-app/src/App.js
@@ -1,40 +1,18 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+//https://api.github.com/users/meislifka
 
-function useInput(initialValue) {
-  const [value, setValue] =
-    useState(initialValue);
-  return [
-    {
-      value,
-      onChange: (e) => setValue(e.target.value)
-    },
-    () => setValue(initialValue)
-  ];
-}
+
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] =
-    useState("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(
-      `${titleProps.value}, ${colorProps.value}`
-    );
-    resetTitle();
-    resetColor();
-  };
+  const [data,setData] = useState(null);
+  useEffect(()=>{
+    fetch('https://api.github.com/users/meislifka').then((response)=>response.json())
+    .then(setData);
+  },[]);
+  if(data) return <pre>{JSON.stringify(data,null,2)}</pre>
   return (
-    <form onSubmit={submit}>
-      <input
-        {...titleProps}
-        type="text"
-        placeholder="color title..."
-      />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
+    <h1>Data</h1>  
   );
 }
 
